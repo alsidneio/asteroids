f5a6586bd1966bb6afe14ae81475fc3f9be799b6
alsidneio <alsidneio.bell@omes.ok.gov>
2025-03-03 16:53:45 -0600

    feat: added rotation motion to the Player object added a player object with a few method overrides updated the main method to render a white triangle

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 from constants import *
 import constants
+from player import Player
 
 
 def main():
     print("Starting Asteroids!")
     pygame.init()
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = game_clock.tick(60) / 1000
+        player.update(dt)
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
