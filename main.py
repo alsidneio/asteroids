9d7d4e095f8d07ce5f00fc5f95c5c01be60b921f
alsidneio <alsidneio.bell@omes.ok.gov>
2025-03-04 16:33:47 -0600

    feat: implemented the asteroid and asteroid fields

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import *
 import constants
 from player import Player
+from asteroid import Asteroid
+from astroidfield import AsteroidField
 
 
 def main():
@@ -9,18 +11,29 @@ def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
-
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0, 0, 0))
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
-        player.update(dt)
+        updatable.update(dt)
+
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
